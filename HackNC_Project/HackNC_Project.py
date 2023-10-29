a9f4e42a028e8f9fe9c638a7987551dc3ff9f898
Steve <77592670+vondoste@users.noreply.github.com>
2023-10-28 20:32:25 -0400

    first baby steps

--- HackNC_Project/HackNC_Project.py
+++ HackNC_Project/HackNC_Project.py
@@ -1,7 +1,34 @@
+"""
+HackNC 2023 project
+Team Members: Thomas Kung, Kevin Hayes, and Steven von Dohlen
+
+
+
+"""
+
+from tkinter import *
+from container import Container
+import pandas
+
 
 def main():
-    #do something
-    print("Hello")
+    # define the window
+    
+ 
+    root = Tk()
+ 
+    #optional
+    root.configure(background='yellow')
+    root.geometry('600x800')
+ 
+    #place any of your code lines here
+ 
+    #end of code
+    root.mainloop()
+
+    md1 = Container()
+    print(md1.open)
+    
 
 if __name__=="__main__":
     main()
---
+++ HackNC_Project/container.py
@@ -0,0 +1,44 @@
+import datetime
+
+class Container:
+  def __init__(self,med_name : str = [], open_by : datetime.datetime = datetime.datetime.now()):
+    self.med_name = med_name
+    self.open = False
+    self.open_by = open_by
+
+  def __str__(self):
+    return "medicine name: %s is open:%s open by:%s"%(self.med_name, self.open, self.open_by)
+
+  def med_late(self):
+    return self.open_by < datetime.datetime.now()
+  
+  def is_open(self):
+    return self.open
+
+  def open_container(self):
+    if self.open:
+      return "container is already open"
+    else:
+      self.open = True
+      return self.med_name
+  def close_container(self):
+    if self.open:
+      self.open = False
+      return "Door Closed"
+    else:
+      return "Door is already closed"
+
+  def insert_medicine(self, medicine):
+    if self.open:
+      self.med_name.append(medicine)
+      return "Medicine inserted"
+    else:
+      return "door is closed. Operation failed."
+
+  def remove_medicine(self):
+    if self.open:
+      tmp = self.med_name
+      self.med_name = []
+      return tmp
+    else:
+      return "Door is closed. Operation failed."
